cfeb32e376521b46d803d7a39c51fe50741d47c3
nico3117 <nicobaca17@gmail.com>
2023-01-12 11:13:01 +0100

    add access cors x-access-token

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express(); // creation de l'application grace au framework
 // Passby CORS errors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, UserID, Email');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, x-access-token, Authorization, UserID, Email');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
