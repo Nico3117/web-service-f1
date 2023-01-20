b4482cd16bb3f238fd02481f46805c7cebb67a24
BenBdlt <bordelet.benjamin@free.fr>
2023-01-20 09:05:32 +0100

    fix erreur log

--- controllers/circuit.js
+++ controllers/circuit.js
@@ -1,19 +1,20 @@
 const Circuit = require('../models/circuit');
+const logger = require("../logger");
 
 exports.getCircuits = (req, res) => {
     Circuit.find()
         .then((circuits) => res.status(200).json(circuits))
         .then(logger.info(`Récupération des circuits`))
         .catch((err) => res.status(400).json('Error: ' + err))
-        .catch(logger.error(`Erreur getCircuits: ${err}`));
+        .catch(logger.error(`Erreur getCircuits`));
 };
 
 exports.getCircuitById = (req, res) => {
     Circuit.findById(req.params.id)
         .then((circuit) => res.status(200).json(circuit))
-        .then(logger.info(`Récupération du circuit: ${circuit.id}`))
+        .then(logger.info(`Récupération du circuit`))
         .catch((err) => res.status(400).json('Error: ' + err))
-        .catch(logger.error(`Erreur getCircuitById: ${err}`));
+        .catch(logger.error(`Erreur getCircuitById`));
 };
 
 exports.createCircuit = (req, res) => {
@@ -33,9 +34,9 @@ exports.createCircuit = (req, res) => {
     
     newCircuit.save()
         .then((saved) => res.status(200).json(saved))
-        .then(logger.info(`Création circuit: ${circuit.id}`))
+        .then(logger.info(`Création circuit:`))
         .catch((err) => res.status(500).json('Error: ' + err))
-        .catch(logger.error(`Erreur createCircuit: ${err}`));
+        .catch(logger.error(`Erreur createCircuit:`));
 };
 
 exports.updateCircuit = (req, res) => {
@@ -47,9 +48,9 @@ exports.updateCircuit = (req, res) => {
                 .then((result) => res.status(200).json(result))
                 .catch((err) => res.status(500).json('Error: ' + err))
         })
-        .then(logger.info(`Création circuit: ${circuit.id}`))
+        .then(logger.info(`Création circuit`))
         .catch((err) => res.status(404).json('Error: ' + err))
-        .catch(logger.error(`Erreur updateCircuit: ${err}`));
+        .catch(logger.error(`Erreur updateCircuit:`));
 };
 
 exports.deleteCircuit = (req, res) => {
